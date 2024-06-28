30f0824e016538b2f48e50d4621969dbad9945eb
gotoa1234 <cap8825@gmail.com>
2024-06-28 10:07:16 +0800

    1. 移除中文，.bat 執行檔案只支援 ANSI 編碼

--- assets/download/GitBatchFile/GitCherryPickCompare.bat
+++ assets/download/GitBatchFile/GitCherryPickCompare.bat
@@ -1,32 +1,32 @@
 @echo off
 setlocal
 
-REM 1. 輸入來源分支
+REM 1. Input source branch.
 set /p source_branch="Enter source branch name: "
 
-REM 2. 輸入目標分支
+REM 2. Input target branch.
 set /p target_branch="Enter target branch name: "
 
-REM 3. 找出"來源分支"不在"目標分支"的簽入
+REM 3. Find out not in source branch committed.
 for /f "tokens=*" %%i in ('git cherry %target_branch% %source_branch% ^| findstr /B "+"') do (
     set "commit=%%i"
-	REM 4-1. 呼叫比對函式
+	REM 4-1. Call compare method.
     call :process_commit 
 )
 
-REM 5. 找完後等待跳出
+REM 5. Wait break when find out finished.
 endlocal
 pause 
 exit /b
 
-REM 4-2. 執行比對
+REM 4-2. Execute compare method.
 :process_commit
-REM 4-3. 提取出簽入的 Git Hash
+REM 4-3. Get committed git hash.
 for /f "tokens=2" %%a in ("%commit%") do (
     set "commit_hash=%%a"
 )
 
-REM 4-4. 找出的來源 Git Hash 比對是否在 目標的Log中出現
+REM 4-4. Get Log of source branch committed git hash.
 git log %target_branch% | findstr /C:"%commit_hash%" >nul
 if %errorlevel%==0 (
     echo Commit %commit_hash% has been cherry-picked.
